172670e87cb56eff1ccb6d521fe5b41efd11a648
amahcan562 <mcandres@iesrafaelalberti.es>
2023-11-23 21:23:51 +0100

    Cadenas

--- src/Cadenas/ej3_01.py
+++ src/Cadenas/ej3_01.py
@@ -1,5 +1,11 @@
 def main():
-    print("")
-    
+    cadena = "Mi nombre es Andrés."
+    longitud = len(cadena)
+
+    while longitud > 0:
+        longitud -= 1
+        print(cadena[longitud])
+        
+
 if __name__ == "__main__":
     main() 
--- src/Cadenas/ej3_04.py
+++ src/Cadenas/ej3_04.py
@@ -1,5 +1,25 @@
+def contar_letras_en_palabra(palabra: str, letra_a_contar: str) -> int:
+    contador = palabra.count(letra_a_contar)
+    return contador
+
+def pedir_palabra_y_letra() -> tuple:
+    palabra = input("Introduzca una palabra: ")
+    letra = None
+    while letra is None:
+        try:
+            letra = input("Indique la letra que quiere contar: ")
+            if not letra.isalpha():
+                raise NameError
+        except NameError:
+            print("ERROR! Has introducido un número.")
+            letra = None
+    return palabra, letra
+
 def main():
-    print("tontito")
+    palabra, letra = pedir_palabra_y_letra()
+    print(contar_letras_en_palabra(palabra, letra))
+
+    #print(contar_letras_en_palabra(pedir_palabra_y_letra()))
     
 if __name__ == "__main__":
     main() 
